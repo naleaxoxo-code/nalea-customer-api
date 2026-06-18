bf2aee65709d48c66e10c37e1e9351fc82c5326a
naleaxoxo-code <naleaxoxo@gmail.com>
2026-06-18 20:18:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const multer  = require('multer');
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 5 * 1024 * 1024 } });
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 const SHOPIFY_STORE         = process.env.SHOPIFY_STORE;
